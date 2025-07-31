eebbbd6648afaf4165dbd1648d3e5615ea610e30
asmithshelton <77249555+asmithshelton@users.noreply.github.com>
2025-07-31 12:17:25 -0400

    Update finanalysis.py

--- finanalysis.py
+++ finanalysis.py
@@ -6,8 +6,8 @@ import streamlit as st
 def fetch_data(tickers, start_date, end_date):
 data_frames = []
 for ticker in tickers:
-stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
-data_frames.append(stock_data)
+  stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
+  data_frames.append(stock_data)
 
 data = pd.concat(data_frames, axis=1, keys=tickers)
   data.columns = tickers
